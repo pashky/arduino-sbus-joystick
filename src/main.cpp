2ebbd59e41aa77f2bd2db7e74c723ec7c92310ac
Pavel Kurnosov <pashky@gmail.com>
2020-12-27 23:55:27 +0000

    Simplify

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,6 @@ Joystick_ Joystick;
 
 SBUS sbus(Serial1);
 
-const unsigned long gcAnalogDelta = 25;
-unsigned long gNextTime = 0;
-
-uint16_t t;
-uint16_t a;
-uint16_t e;
-uint16_t r;
-uint16_t c5;
-uint16_t c6;
-uint16_t c7;
-
 uint16_t channels[16];
 bool failSafe;
 bool lostFrame;
@@ -37,27 +26,11 @@ void setup() {
 
 void loop() {
   if(sbus.read(&channels[0], &failSafe, &lostFrame)) {
-    t = channels[0];
-    a = channels[1];
-    e = channels[2];
-    r = channels[3];
-    c5 = channels[4];
-    c6 = channels[5];
-    c7 = channels[6];
+    Joystick.setXAxis(channels[0]);
+    Joystick.setYAxis(channels[1]);
+    Joystick.setRxAxis(channels[2]);
+    Joystick.setRyAxis(channels[3]);
+//    Joystick.setZAxis(channel[4]);
+//    Joystick.setRzAxis(channel[5]);
   }
-
-  // Write priority SBUS channels (TAER) to controller
-  Joystick.setXAxis(t);
-  Joystick.setYAxis(a);
-  Joystick.setRxAxis(e);
-  Joystick.setRyAxis(r);
-
-  // Write remaining SBUS channels to controller
-  // if (millis() >= gNextTime) {
-  //   gNextTime = millis() + gcAnalogDelta;
-
-  //   Joystick.setZAxis(c5);
-  //   Joystick.setRzAxis(c6);
-  //   Joystick.setButton(0, map(c7, 172, 1811, 0, 1));
-  // }
 }
